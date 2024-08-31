56828dd63831af0ff68d0ea5c12602167773ce4a
Mi PC <Mi PC@LAPTOP-GV315B0F>
2024-08-30 21:57:29 -0500

    Mi primer repositorio 1.0

---
+++ src/main/java/com/mycompany/miprimerrepositorio/MiPrimerRepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miprimerrepositorio;
+
+/**
+ *
+ * @author Camilo David Pacheco Prieto
+ */
+public class MiPrimerRepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hola este es mi primer repositorio");
+    }
+}
